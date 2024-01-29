6e59025587a540185c4153fc75541f275293ac7b
Abhinav Kumar <abhinavkr.2108@gmail.com>
2024-01-29 18:51:18 +0530

    Create auth and header pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Signout from "./pages/signout/Signout";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="flex items-center justify-center h-screen w-screen">
-      <h1 className="text-green-500">Hello Vite + React</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signout" element={<Signout />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
