c570d40d612e32b69f5a906d698ae6ff2eb511e7
Abhinav Kumar <abhinavkr.2108@gmail.com>
2024-01-31 11:50:43 +0530

    Added redux persist to store data in local storage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react";
-import { store } from "./redux/Store.jsx";
+import { persistor, store } from "./redux/Store.jsx";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </PersistGate>
   </Provider>
 );
--- frontend/src/redux/Store.jsx
+++ frontend/src/redux/Store.jsx
@@ -1,13 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/UserSlice.jsx"; // Export default so we can change the name
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/lib/persistStore";
 
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  version: 1,
+};
+
+const persistReducerUser = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistReducerUser,
+
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware({
       serializableCheck: false,
     });
   },
 });
+
+export const persistor = persistStore(store);
